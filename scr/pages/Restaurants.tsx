fc5b4e1fcdfc31232e0f70e8ae21ed4958fe8061
AJR02 <AJR02@user.noreply.github.com>
2025-11-15 16:18:27 +0300

    add initial imports for React and components

---
+++ scr/pages/Restaurants.tsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+import RestaurantCard from "@/components/RestaurantCard";
+import { useRestaurants } from "@/hooks/use-restaurants";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
