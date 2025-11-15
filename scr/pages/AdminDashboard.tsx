a45047e9753a85bff94091131a6fc9dc45f23c66
Mundiaa <mundiamanuel5@gmail.com>
2025-11-15 14:14:50 +0300

    modified

---
+++ scr/pages/AdminDashboard.tsx
@@ -0,0 +1,4 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Users, Building2, BookOpen, Settings } from "lucide-react";
